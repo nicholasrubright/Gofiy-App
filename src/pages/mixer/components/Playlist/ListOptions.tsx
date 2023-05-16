ba0588ca3a5bdfc3cc3f2cbab081f61d8677589e
Nicholas Rubright <nicholas@Nicholas-MacBook-Pro.local>
2023-05-16 16:32:40 -0400

    finished buttons for mixer options

--- src/pages/mixer/components/Playlist/ListOptions.tsx
+++ src/pages/mixer/components/Playlist/ListOptions.tsx
@@ -1,3 +1,42 @@
+import { useState } from "react";
+
+const SelectAllButton = (props: SelectAllButtonProps) => {
+  const { active } = props;
+
+  if (active) {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        className="bi bi-check-square-fill"
+        viewBox="0 0 16 16"
+      >
+        <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm10.03 4.97a.75.75 0 0 1 .011 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.75.75 0 0 1 1.08-.022z" />
+      </svg>
+    );
+  } else {
+    return (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="16"
+        height="16"
+        fill="currentColor"
+        className="bi bi-check-square"
+        viewBox="0 0 16 16"
+      >
+        <path d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z" />
+        <path d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z" />
+      </svg>
+    );
+  }
+};
+
+interface SelectAllButtonProps {
+  active: boolean;
+}
+
 const Button = () => {
   return (
     <svg
@@ -33,17 +72,23 @@ const ExitButton = () => {
 };
 
 export default function ListOptions() {
+  const [selectAllActive, setSelectAllActive] = useState(false);
+
   return (
     <div className="container">
       <div className="btn-group" role="group">
         <button type="button" className="btn btn-outline-primary">
           <Button />
         </button>
-        <button type="button" className="btn btn-outline-primary">
-          <ExitButton />
+        <button
+          type="button"
+          className="btn btn-outline-primary"
+          onClick={() => setSelectAllActive(!selectAllActive)}
+        >
+          <SelectAllButton active={selectAllActive} />
         </button>
         <button type="button" className="btn btn-outline-primary">
-          <Button />
+          <ExitButton />
         </button>
       </div>
     </div>
